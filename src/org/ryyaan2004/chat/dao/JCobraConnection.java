13a10c8eb2d4af41f1e26a1736be45b2fff355a9
ryyaan2004 <ryyaan_2004@yahoo.com>
2015-09-08 15:52:49 -0500

    Uses DataSource instead of direct jdbc connections

--- src/org/ryyaan2004/chat/dao/JCobraConnection.java
+++ src/org/ryyaan2004/chat/dao/JCobraConnection.java
@@ -1,46 +1,32 @@
 package org.ryyaan2004.chat.dao;
 
-import java.io.FileNotFoundException;
-import java.io.InputStream;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import org.apache.log4j.Logger;
 
 public class JCobraConnection {
-
-  public static Connection getConnection() throws SQLException {
-
-    final String USER_KEY = "user";
-    final String PW_KEY = "password";
-    final String CONNURL_KEY = "connUrl";
-
-    Properties props = new Properties();
-    String connUrl = getConnectionProps().getProperty( CONNURL_KEY );
-    props.setProperty( USER_KEY, getConnectionProps().getProperty( USER_KEY ) );
-    props.setProperty( PW_KEY, getConnectionProps().getProperty( PW_KEY ) );
-
-    Connection conn = DriverManager.getConnection( connUrl, props );
-
-    return conn;
+  private static Logger log = Logger.getLogger(JCobraConnection.class);
+  private static DataSource ds = null;
+  private static String jCobraChatJndi = "java:/jcobrachatDs";
+  
+  static
+  {
+	  try 
+	  {
+		ds = (DataSource) InitialContext.doLookup(jCobraChatJndi);
+	  } 
+	  catch (NamingException ne) 
+	  {
+		log.error("In JCobraConnection static initialization block: The datasource lookup failed",ne);
+	  }
   }
-
-  private static Properties getConnectionProps() {
-    Properties props = new Properties();
-    String propFileName = "/dataSource.properties";
-
-    try ( InputStream inputStream = JCobraConnection.class.getResourceAsStream( propFileName ); ) {
-
-      if ( inputStream != null ) {
-        props.load( inputStream );
-      }
-      else {
-        throw new FileNotFoundException( "property file '" + propFileName + "' not found in the classpath" );
-      }
-    }
-    catch ( Exception ex ) {
-      ex.printStackTrace();
-    }
-    return props;
+  
+  public static Connection getConnection() throws SQLException {
+    return ds.getConnection();
   }
 }
